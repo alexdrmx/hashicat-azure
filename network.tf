1a6bf8118f4adc3a19f0fe68cd4f529a3ea08da6
Alexandre <alex.desreumaux.20@gmail.com>
2022-12-15 12:42:35 +0000

    network 3

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   version = "3.5.0"
   source  = "app.terraform.io/adesreumaux/network/azurerm"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
